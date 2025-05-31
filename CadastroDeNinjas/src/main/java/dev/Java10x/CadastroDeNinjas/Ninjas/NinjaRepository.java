8671c9dad16be8bf30378159eaa21a2d946b80ef
marcotulio <marcotuliosp4@gmail.com>
2025-05-31 08:10:13 -0300

    feat: adicionando  controllers,services,repository das calsses

---
+++ CadastroDeNinjas/src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -0,0 +1,8 @@
+package dev.Java10x.CadastroDeNinjas.Ninjas;
+
+import jakarta.persistence.Id;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel,Long> {
+}
