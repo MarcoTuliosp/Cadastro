28a03e7a3c7adcdbe7044a4656459c6f41b6f1bb
marcotulio <marcotuliosp4@gmail.com>
2025-07-26 04:06:08 -0300

    fix: inclui a annotations @JsonIgnore para previnir o erro de serealização

--- CadastroDeNinjas/src/main/java/dev/Java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ CadastroDeNinjas/src/main/java/dev/Java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,5 +1,6 @@
 package dev.Java10x.CadastroDeNinjas.Missoes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.Java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.*;
@@ -22,6 +23,7 @@ public class MissoesModel {
 
     //OneToMany -> Uma missao pode ter varios ninjas
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore
     private List<NinjaModel> ninjas;
 
 
